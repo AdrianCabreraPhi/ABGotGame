404fa3dd5e9e6fe2dfbf5ed0ddcb615e98b1428e
AdrianCabreraPhi <adriancabproven@gmail.com>
2023-12-22 13:30:42 +0100

    contact mail changed

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -7,14 +7,14 @@ import { FaPencilAlt } from "react-icons/fa";
 import "./ContactMe.css";
 
 export function ContactMe() {
-  const email = "abgames@gmail.com";
+  const email = "business@abgotgame.com";
   const handleEmailClick = () => {
     window.location.href=`mailto:${email}`;
   }
 
   return (
     <>
-    <Tooltip label="abgames@gmail.com" bg='red.600' fontSize="md">
+    <Tooltip label="business@abgotgame.com" bg='red.600' fontSize="md">
     <Button onClick={handleEmailClick} className="btn" size="lg">
         <FaPencilAlt />
         <Text ml={2}>Contact Me</Text>
